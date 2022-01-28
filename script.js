29a1fa9ab05ac2baaafe7aff5ab7c768726d1b03
Naveen Varatharaj <88475979+NaveenVaratharaj@users.noreply.github.com>
2022-01-28 19:28:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,31 +18,31 @@ for (var i=0; i<totaldrums; i++){
     function makesound(key) {
         switch(key){
             case "w":
-                var audio1 = new Audio('sounds/crash.mp3');
+                var audio1 = new Audio('crash.mp3');
                 audio1.play();
             break;
             case "a":
-                var audio2 = new Audio('sounds/kick-bass.mp3');
+                var audio2 = new Audio('kick-bass.mp3');
                 audio2.play();
             break;
             case "s":
-                var audio3 = new Audio('sounds/snare.mp3');
+                var audio3 = new Audio('snare.mp3');
                 audio3.play();
             break;
             case "d":
-                var audio4 = new Audio('sounds/tom-1.mp3');
+                var audio4 = new Audio('tom-1.mp3');
                 audio4.play();
             break;
             case "j":
-                var audio5 = new Audio('sounds/tom-2.mp3');
+                var audio5 = new Audio('tom-2.mp3');
                 audio5.play();
             break;
             case "k":
-                var audio6 = new Audio('sounds/tom-3.mp3');
+                var audio6 = new Audio('tom-3.mp3');
                 audio6.play();
             break;
             case "l":
-                var audio7 = new Audio('sounds/tom-1.mp3');
+                var audio7 = new Audio('tom-1.mp3');
                 audio7.play();
             break;
             default:
